a7a50931d9fa2175f4405c163c9222b9ad71227b
Richard Lopes <richard.lopes92@gmail.com>
2019-03-28 19:57:46 -0300

    Fetch functions for youtube api

---
+++ src/services/fetchPlaylist.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+import cache from "./cache";
+
+// TODO replace api key
+const API_KEY = "YOUR_API_KEY_HERE";
+const PLAYLIST = "PL6t93nUFQQ1ZiXMfhPyhjb0PX3LgEVMcF";
+const MAX_RESULTS = 5;
+const URL_BASE = `https://www.googleapis.com/youtube/v3/playlistItems`;
+const PART = "snippet";
+
+// eslint-disable-next-line no-underscore-dangle
+const __fetchPlaylist = async ({ pageToken } = {}) => {
+  const url = `${URL_BASE}?part=${PART}&maxResults=${MAX_RESULTS}&playlistId=${PLAYLIST}&key=${API_KEY}${
+    pageToken ? `&pageToken=${pageToken}` : ""
+  }`;
+
+  const response = await axios.get(encodeURI(url));
+  const { items } = response.data;
+
+  cache.playlist = response.data;
+  cache.playlistLength = response.data.pageInfo.totalResults;
+
+  return items;
+};
+
+export default async function fetchPlaylist() {
+  return __fetchPlaylist();
+}
+
+fetchPlaylist.next = async () => {
+  const pageToken = cache.playlist.nextPageToken;
+  return __fetchPlaylist({ pageToken });
+};
